c2911829cb88925dad3c64af5f264a5910a2922c
刘毅 <liuyi@liuyideMac-mini.local>
2015-11-01 22:16:57 +0800

    aaa

--- helloapp/main.go
+++ helloapp/main.go
@@ -8,6 +8,6 @@ import (
 )
 
 func main() {
-	class.TestROM
+	class.TestORM()
 	beego.Run()
 }
--- helloapp/models/db.go
+++ helloapp/models/db.go
@@ -1,16 +1,15 @@
 package models
 
 import (
-"blog/helloapp/models/class"
+	"blog/helloapp/models/class"
 	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func init() {
-	orm.RegisterDriver("mysql", orm.DR_MYSQL)
-    orm.RegisterDataBase("default", "mysql", "root:jkjjyjss(localhost:3306)/
-    	jblog?charset=utf8")
-    orm.RegisterModel(new(class.User))
-    orm.RunSyncdb("default", false, true)
+	orm.RegisterDriver("mysql", orm.DR_MySQL)
+	orm.RegisterDataBase("default", "mysql", "root:jkjjyjss(localhost:3306)/jblog?charset=utf8")
+	orm.RegisterModel(new(class.User))
+	orm.RunSyncdb("default", false, true)
 
 }
