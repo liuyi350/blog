770c4cb3e534850a5d08cb587d47b7b8dc3950f2
liuyi350 <350117156@qq.com>
2015-11-02 12:39:58 +0800

    jj

--- helloapp/models/db.go
+++ helloapp/models/db.go
@@ -8,14 +8,9 @@ import (
 
 func init() {
 	orm.RegisterDriver("mysql", orm.DR_MySQL)
-<<<<<<< HEAD
+
     orm.RegisterDataBase("default", "mysql", "root:jkjjyjss@tcp(localhost:3306)/helloapp?charset=utf8")
     orm.RegisterModel(new(class.User))
     orm.RunSyncdb("default", false, true)
-=======
-	orm.RegisterDataBase("default", "mysql", "root:jkjjyjss(localhost:3306)/jblog?charset=utf8")
-	orm.RegisterModel(new(class.User))
-	orm.RunSyncdb("default", false, true)
->>>>>>> c2911829cb88925dad3c64af5f264a5910a2922c
 
 }
